6f5a52c2415ca951d1a78934cf79751baa73b0e7
Rihab ATTIG <sherlockHolmesRA@users.noreply.github.com>
2022-09-26 11:46:06 +0200

    payment app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
